5b023bf637c922aeda9841bbf2f8b720fb7cec15
SunixPN <paxa14092005@gmail.com>
2024-02-24 13:39:50 +0300

    block the next button

--- src/components/screens/Home/Blog/Pagination/Pagination.jsx
+++ src/components/screens/Home/Blog/Pagination/Pagination.jsx
@@ -4,11 +4,15 @@ import { useQuery } from "react-query"
 import { NewsService } from "../../../../../services/NewsService"
 import { useActions } from "../../../../../hooks/useActions"
 import SnackLoader from "./SnackLoader/SnackLoader"
+import SnackBar from "../../../Admin/CreateNews/TextEditor/ControllerList/SnackBar/SnackBar"
 
 const Pagination = forwardRef((__, ref) => {
     const [page, setPage] = useState(1)
     const [active, setActive] = useState({ activePrev: page !== 1, activeNext: true })
     const [first, setFirst] = useState(true)
+    const [errorActive, setErrorActive] = useState(false)
+    const [firstQuery, setFirstQuery] = useState(true)
+    const [endPage, setEndPage] = useState(null)
 
     const { initialNews } = useActions()
 
@@ -21,7 +25,19 @@ const Pagination = forwardRef((__, ref) => {
     useEffect(() => {
         if (dataNews) {
 
-            if (first) {
+            if (dataNews.news.length === 0) {
+
+                if (firstQuery) {
+                    setEndPage(page)
+                    setErrorActive(true)
+                    setFirstQuery(false)
+                    setPage(prev => prev - 1)
+                }
+
+                setActive({...active, activeNext: false})
+            }
+
+            else if (first) {
                 initialNews(dataNews.news)
                 setFirst(false)
             }
@@ -36,8 +52,10 @@ const Pagination = forwardRef((__, ref) => {
     const prevHandleClick = () => {
         if (page !== 1) {
             ref.current.scrollIntoView({ block: "start" })
+            setActive({...active, activeNext: true})
+
             if (page === 2) {
-                setActive({...active, activePrev: false})
+                setActive({activeNext: true, activePrev: false})
             }
 
             setPage(prev => prev -1)
@@ -45,13 +63,24 @@ const Pagination = forwardRef((__, ref) => {
     }
 
     const nextHandleClick = () => {
-        ref.current.scrollIntoView({ block: "start" })
-        setPage(prev => prev + 1)
-        setActive({...active, activePrev: true})
+        if (page === endPage - 2 && active.activeNext) {
+            ref.current.scrollIntoView({ block: "start" })
+            setActive({activePrev: true, activeNext: false})
+            setPage(prev => prev + 1)
+        }
+
+        else if (active.activeNext) {
+            ref.current.scrollIntoView({ block: "start" })
+            setPage(prev => prev + 1)
+            setActive({...active, activePrev: true})
+        }
+
+      
     }
 
     return (
         <>
+        <SnackBar open={errorActive} setOpen={setErrorActive} severity={"error"} text={"Больше новостей нету"} />
         <SnackLoader open={isLoading} />
         <div className={styles.pagintaion}>
             <button 
